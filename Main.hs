76b42e2b8741c8414a434b851b3117cdc0f76d65
root <hawk.alan@gmail.com>
2015-05-22 12:24:57 +0000

    update

--- Main.hs
+++ Main.hs
@@ -2,17 +2,19 @@ module Main where
 
 import System.Environment
 import System.IO
+import Control.Applicative
 
 help = "-d to dos, -u to unix"
 
 main = do 
   args <- getArgs
   let line_mode = case args of  {
-      ["-u"] -> Just LF;
-      ["-d"] -> Just CRLF ;
-      _ -> Nothing }
+   ["-u"] -> Just LF;
+   ["-d"] -> Just CRLF;
+   _ -> Nothing }
   case line_mode of 
     Nothing -> putStrLn help
-    Just mode -> do hSetNewlineMode stdout (nativeNewlineMode {outputNL = mode})
-                    interact id
-
+    Just mode -> do 
+                 hSetNewlineMode stdin (universalNewlineMode)
+                 hSetNewlineMode stdout (universalNewlineMode {outputNL = mode})
+                 interact id
